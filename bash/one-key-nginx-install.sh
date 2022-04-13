b5637b4cb573046db6dd45fee672fd6f68fd89c7
yutao517 <62100249+yutao517@users.noreply.github.com>
2022-04-13 13:54:06 +0800

    Update one-key-nginx-install.sh

--- bash/one-key-nginx-install.sh
+++ bash/one-key-nginx-install.sh
@@ -4,11 +4,11 @@
 yum -y install zlib zlib-devel openssl openssl-devel pcre pcre-devel gcc gcc-c++ autoconf automake make psmisc net-tools lsof vim geoip geoip-devel wget -y
 
 #新建luogan用户和组
-id  wangyutao || useradd wangyutao -s /sbin/nologin
+id  nginx || useradd nginx -s /sbin/nologin
 
 #下载nginx软件
-mkdir  /wangyutao99 -p
-cd /wangyutao99
+mkdir  /nginx -p
+cd /nginx
 wget  https://nginx.org/download/nginx-1.21.4.tar.gz
 
 #解压软件
@@ -17,7 +17,7 @@ tar xf nginx-1.21.4.tar.gz
 cd nginx-1.21.4
 
 #编译前的配置
-./configure --prefix=/usr/local/scwangyutao99  --user=wangyutao --group=wangyutao  --with-http_ssl_module   --with-threads  --with-http_v2_module  --with-http_stub_status_module  --with-stream  --with-http_geoip_module --with-http_gunzip_module --with-http_realip_module --with-http_stub_status_module --with-http_ssl_module 
+./configure --prefix=/usr/local/nginx  --user=nginx --group=nginx  --with-http_ssl_module   --with-threads  --with-http_v2_module  --with-http_stub_status_module  --with-stream  --with-http_geoip_module --with-http_gunzip_module --with-http_realip_module --with-http_stub_status_module --with-http_ssl_module 
 
 #如果上面的编译前的配置失败，直接退出脚本
 if (( $? != 0));then
@@ -29,7 +29,7 @@ make -j 2
 make  install
 
 #修改PATH变量
-echo  "PATH=$PATH:/usr/local/scwangyutao99/sbin" >>/root/.bashrc
+echo  "PATH=$PATH:/usr/local/nginx/sbin" >>/root/.bashrc
 #执行修改了环境变量的脚本
 source /root/.bashrc
 
@@ -46,17 +46,17 @@ systemctl disable firewalld
 
 #开机启动
 chmod +x /etc/rc.d/rc.local
-echo  "/usr/local/scwangyutao99/sbin/nginx" >>/etc/rc.local
+echo  "/usr/local/nginx/sbin/nginx" >>/etc/rc.local
 
 #修改nginx.conf的配置，例如：端口号，worker进程数，线程数，服务域名
 
-sed  -i '/worker_processes/ s/1/2/' /usr/local/scwangyutao99/conf/nginx.conf
-sed  -i  '/worker_connections/ s/1024/2048/' /usr/local/scwangyutao99/conf/nginx.conf
-sed  -i -r '36c \\tlisten  80;' /usr/local/scwangyutao99/conf/nginx.conf
-sed  -i -r '37c \\tserver_name www.yutao.co;' /usr/local/scwangyutao99/conf/nginx.conf
+sed  -i '/worker_processes/ s/1/2/' /usr/local/nginx/conf/nginx.conf
+sed  -i  '/worker_connections/ s/1024/2048/' /usr/local/nginx/conf/nginx.conf
+sed  -i -r '36c \\tlisten  80;' /usr/local/nginx/conf/nginx.conf
+sed  -i -r '37c \\tserver_name www.yutao.co;' /usr/local/nginx/conf/nginx.conf
 
 #killall nginx进程
 killall -9 nginx
 su
 #启动nginx
-/usr/local/scwangyutao99/sbin/nginx
+/usr/local/nginx/sbin/nginx
